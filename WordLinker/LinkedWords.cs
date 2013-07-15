6af567171da43d6915d96e29e9c96c9938fdfe82
jdwright217 <jdwright@mtu.edu>
2013-07-15 18:56:56 -0400

    Finished functionality to allow the choosing of file or existing xml to create or append to. Can review links before they are flushed to the new/existing xml. Can choose the txt file to import links.

--- WordLinker/LinkedWords.cs
+++ WordLinker/LinkedWords.cs
@@ -11,5 +11,10 @@ namespace WordLinkerTool
         public string FirstWord { get; set; }
         public string SecondWord { get; set; }
 
+        public override string ToString()
+        {
+            return FirstWord + "\t\t" + SecondWord;
+        }
+
     }
 }
